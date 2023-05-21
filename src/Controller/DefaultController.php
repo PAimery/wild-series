828998c9feaca2f8ae50ebc2b98e02b9cd3b2502
Aimery Pineau <aimery.pin@gmail.com>
2023-05-21 02:46:21 +0200

    challenge

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/default/', name: 'app_index')]
+    #[Route('/default/', name: 'Accueil_index')]
     public function index(): Response
     {
         return $this->render('default/index.html.twig', [
